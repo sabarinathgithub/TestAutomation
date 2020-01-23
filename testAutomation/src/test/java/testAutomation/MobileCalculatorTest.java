5eec9da7c67c6ed700289d9cb46fa77ff549e2c0
15132 <15132@LAPTOP-1IPAD3CB.attlocal.net>
2020-01-22 21:00:09 -0600

    Appium Test - Calculator Addition

--- testAutomation/src/test/java/testAutomation/MobileCalculatorTest.java
+++ testAutomation/src/test/java/testAutomation/MobileCalculatorTest.java
@@ -2,7 +2,11 @@ package testAutomation;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.asserts.Assertion;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
@@ -10,9 +14,15 @@ public class MobileCalculatorTest {
 	
 	static AppiumDriver<MobileElement> driver;
 
-	public static void main(String[] args) throws MalformedURLException {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {
+		try {
+			openCalculator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public static void openCalculator() throws Exception {
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("deviceName", "Moto E4 Plus");
 		cap.setCapability("uid", "ZY2258RPB2");
@@ -23,7 +33,39 @@ public class MobileCalculatorTest {
 		
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AppiumDriver<MobileElement>(url, cap);
-		System.out.println("Appium started the Application successfully...");
+		System.out.println("Appium started the Application successfully...");	
+		Thread.sleep(500);
+		
+		MobileElement two = driver.findElement(By.id("com.google.android.calculator:id/digit_2"));
+		MobileElement plus = driver.findElement(By.id("com.google.android.calculator:id/op_add"));
+		MobileElement six = driver.findElement(By.id("com.google.android.calculator:id/digit_6"));
+		MobileElement equals = driver.findElement(By.id("com.google.android.calculator:id/eq"));
+		
+		two.click();
+		plus.click();
+		six.click();
+		equals.click();
+		
+		MobileElement total = driver.findElement(By.id("com.google.android.calculator:id/result_final"));
+		String result = total.getText();
+		System.out.println("Result is: "+ result);
+	}
+	
+	private static void additionTest() {
+		MobileElement two = driver.findElement(By.id("	com.google.android.calculator:id/digit_2"));
+		MobileElement plus = driver.findElement(By.id("com.google.android.calculator:id/op_add"));
+		MobileElement six = driver.findElement(By.id("com.google.android.calculator:id/digit_6"));
+		MobileElement equals = driver.findElement(By.id("com.google.android.calculator:id/eq"));
+		MobileElement total = driver.findElement(By.className("android.widget.TextView"));
+		
+		two.click();
+		plus.click();
+		six.click();
+		equals.click();
+		
+		String result = total.getText();
+		System.out.println("Result is: "+ result);
+		System.out.println("Completed...");
 	}
 
 }
